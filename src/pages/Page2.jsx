73979902dfdde6dceb7c052ce141bcbe68c4fe5c
Susanta Ray <susantaray786@gmail.com>
2024-12-11 21:13:53 +0530

    .....

--- src/pages/Page2.jsx
+++ src/pages/Page2.jsx
@@ -1,9 +1,23 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Page2 = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   gsap.registerPlugin(ScrollTrigger);
   useGSAP(() => {
     gsap.from(".rotateText", {
@@ -28,32 +42,35 @@ const Page2 = () => {
     animation();
   });
 
-  return (
-    <div id="section2" className="bg-white text-center p-20 overflow-x-hidden">
-      <h3 className="text-gray-500 text-lg font-[font4]">
+  const mobileView = (
+    <div
+      id="section2"
+      className="bg-white text-center px-[10vw] py-[10vw] overflow-x-hidden"
+    >
+      <h3 className="text-gray-500 text-[3.5vw] font-[font4]">
         ©InertiaWebTechnologies | designed and developed by @susantaRay786
       </h3>
-      <div className="rotateText mt-40">
+      <div className="mt-20">
         <h1 className="text-[40vw] text-black font-[font1] leading-[33vw]">
           DESIGN
         </h1>
       </div>
-      <div className="rotateText">
+      <div className="">
         <h1 className="text-[40vw] text-black font-[font1] leading-[33vw]">
           THAT
         </h1>
       </div>
-      <div className="rotateText">
+      <div className="">
         <h1 className="text-[40vw] text-black font-[font1] leading-[33vw]">
           DELIVERS
         </h1>
       </div>
-      <div className="rotateText">
+      <div className="">
         <h1 className="text-[40vw] text-black font-[font1] leading-[33vw]">
           SOLUTIONS
         </h1>
       </div>
-      <div className="rotateText">
+      <div className="">
         <h1 className="text-[40vw] text-black font-[font1] leading-[33vw]">
           THAT
         </h1>
@@ -71,13 +88,82 @@ const Page2 = () => {
           src="/svgs/wordpress.svg"
           alt="wordpress logo"
         />
-        <img className="w-[4vw]" src="/svgs/shopify.svg" alt="shopify logo" />
-        <img className="w-[4vw]" src="/svgs/react.svg" alt="react logo" />
-        <img className="w-[4vw]" src="/svgs/redux.svg" alt="redux logo" />
-        <img className="w-[4vw]" src="/svgs/mongodb.svg" alt="mongoDB logo" />
+        <img className="w-[8vw]" src="/svgs/shopify.svg" alt="shopify logo" />
+        <img className="w-[8vw]" src="/svgs/react.svg" alt="react logo" />
+        <img className="w-[8vw]" src="/svgs/redux.svg" alt="redux logo" />
+        <img className="w-[8vw]" src="/svgs/mongodb.svg" alt="mongoDB logo" />
       </div>
-      <div className="h-[2px] w-1/3 relative left-1/2 -translate-x-1/2 mt-32 bg-black"></div>
+      <div className="h-[2px] w-full relative left-1/2 -translate-x-1/2 mt-8 bg-black"></div>
     </div>
+  );
+
+  return (
+    <>
+      {isMobile ? (
+        mobileView
+      ) : (
+        <div
+          id="section2"
+          className="bg-white text-center p-20 overflow-x-hidden"
+        >
+          <h3 className="text-gray-500 text-lg font-[font4]">
+            ©InertiaWebTechnologies | designed and developed by @susantaRay786
+          </h3>
+          <div className="rotateText mt-40">
+            <h1 className="text-[40vw] text-black font-[font1] leading-[33vw]">
+              DESIGN
+            </h1>
+          </div>
+          <div className="rotateText">
+            <h1 className="text-[40vw] text-black font-[font1] leading-[33vw]">
+              THAT
+            </h1>
+          </div>
+          <div className="rotateText">
+            <h1 className="text-[40vw] text-black font-[font1] leading-[33vw]">
+              DELIVERS
+            </h1>
+          </div>
+          <div className="rotateText">
+            <h1 className="text-[40vw] text-black font-[font1] leading-[33vw]">
+              SOLUTIONS
+            </h1>
+          </div>
+          <div className="rotateText">
+            <h1 className="text-[40vw] text-black font-[font1] leading-[33vw]">
+              THAT
+            </h1>
+          </div>
+          <div className="rotateText relative">
+            {/* <canvas className="h-full -translate-y-8 w-[80%] absolute left-1/2 -translate-x-1/2 bg-red-600"></canvas> */}
+            <h1 className="text-[40vw] text-black font-[font1] leading-[33vw]">
+              CONNECTS
+            </h1>
+          </div>
+
+          <div className="flex w-full items-center justify-center gap-[10vw] mt-10">
+            <img
+              className="w-[4vw]"
+              src="/svgs/wordpress.svg"
+              alt="wordpress logo"
+            />
+            <img
+              className="w-[4vw]"
+              src="/svgs/shopify.svg"
+              alt="shopify logo"
+            />
+            <img className="w-[4vw]" src="/svgs/react.svg" alt="react logo" />
+            <img className="w-[4vw]" src="/svgs/redux.svg" alt="redux logo" />
+            <img
+              className="w-[4vw]"
+              src="/svgs/mongodb.svg"
+              alt="mongoDB logo"
+            />
+          </div>
+          <div className="h-[2px] w-1/3 relative left-1/2 -translate-x-1/2 mt-32 bg-black"></div>
+        </div>
+      )}
+    </>
   );
 };
 
